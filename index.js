37386e86bc6e1b32a3be2bf3acd861a4a2570458
Dharmareddy8520 <119721267+Dharmareddy8520@users.noreply.github.com>
2023-03-07 19:02:10 +0530

    loop

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express=require('express')
 const app=express()
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const ejs=require("ejs");
+app.set("view engine","ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/',function(req,res){
-    res.send("hello world")
+    res.render('index')
 })
-app.listen(3000,function(req,res){
+const port = process.env.PORT || 3000;
+app.listen(port,function(req,res){
     console.log("server started")
 })
