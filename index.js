a938c4a88f7e2f502bfb70e26def21632c6b955b
Dharmareddy8520 <119721267+Dharmareddy8520@users.noreply.github.com>
2023-03-08 22:10:25 +0530

    started

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/',function(req,res){
     res.render('index')
 })
+
+
 const port = process.env.PORT || 3000;
 app.listen(port,function(req,res){
     console.log("server started")
